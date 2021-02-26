6a45ec7d02c85cc1e642c9f222e041adb673bcde
amankumar6 <amankumar786dpsdh@gmail.com>
2021-02-26 14:01:54 +0530

    chore: template

---
+++ public/javascripts/main.js
@@ -0,0 +1,4 @@
+import '../sass/style.scss';
+console.log('hey there');
+
+// import { $, $$ } from './modules/bling';
---
+++ start.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => console.error(err.message))
+
+// READY?! Let's go!
+
+
+const app = require('./app');
+app.set('port', process.env.PORT || 8080);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
